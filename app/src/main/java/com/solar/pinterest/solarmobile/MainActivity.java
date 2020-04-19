6342201aa2bc50268adc7d5c703644b5cf2e8a68
Nell <ADshishova@mail.ru>
2020-04-20 02:05:30 +0300

    profile editing view

--- app/src/main/java/com/solar/pinterest/solarmobile/MainActivity.java
+++ app/src/main/java/com/solar/pinterest/solarmobile/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.your_profile);
+        setContentView(R.layout.your_profile_editing);
     }
 }
