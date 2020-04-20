c36666552ffe1ec8ac94dafeefc9358efa24d4ba
Nell <ADshishova@mail.ru>
2020-04-20 21:25:05 +0300

    added create board design

--- app/src/main/java/com/solar/pinterest/solarmobile/MainActivity.java
+++ app/src/main/java/com/solar/pinterest/solarmobile/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.your_profile_editing);
+        setContentView(R.layout.create_board);
     }
 }
