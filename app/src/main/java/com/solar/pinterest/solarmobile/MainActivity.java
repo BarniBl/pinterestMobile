8d2c8c5b8d231fcc7f61fc63570cce7cd889968d
Nell <ADshishova@mail.ru>
2020-04-20 22:31:41 +0300

    fixed login? reg? adding pin

--- app/src/main/java/com/solar/pinterest/solarmobile/MainActivity.java
+++ app/src/main/java/com/solar/pinterest/solarmobile/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_board);
+        setContentView(R.layout.create_pin);
     }
 }
