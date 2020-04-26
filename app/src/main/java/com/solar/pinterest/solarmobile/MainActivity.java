f8478293c4a90f513301ce4d3ffca3f553341db6
BarniBl <veinskiyva@mail.ru>
2020-04-26 13:43:37 +0300

    added login request

--- app/src/main/java/com/solar/pinterest/solarmobile/MainActivity.java
+++ app/src/main/java/com/solar/pinterest/solarmobile/MainActivity.java
@@ -1,17 +1,40 @@
 package com.solar.pinterest.solarmobile;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.gson.Gson;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import retrofit2.http.GET;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final MediaType JSON_TYPE = MediaType.parse("application/json");
     Button toRegistrationBtn;
     Button loginBtn;
 
@@ -27,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         textInputPassword = findViewById(R.id.login_view_password_layout);
 
         toRegistrationBtn = findViewById(R.id.login_to_registration_button);
+
         toRegistrationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,12 +58,50 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
         loginBtn = findViewById(R.id.login_view_button);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                confirmInput(v);
+                //confirmInput(v);
+                LoginData loginData = new LoginData(textInputEmail.getEditText().getText().toString(), textInputPassword.getEditText().getText().toString());
+
+/*                Network.getInstance().getSolarSunriseApi().login(loginData).enqueue(new retrofit2.Callback<retrofit2.Response>() {
+                    @Override
+                    public void onResponse(retrofit2.Call<retrofit2.Response> call, retrofit2.Response<retrofit2.Response> response) {
+                        System.out.println(response.body().toString());
+                    }
+
+                    @Override
+                    public void onFailure(retrofit2.Call<retrofit2.Response> call, Throwable t) {
+                        t.printStackTrace();
+                    }
+                });*/
+
+                OkHttpClient client = new OkHttpClient();
+                Gson gson = new Gson();
+                String json = gson.toJson(loginData);
+                RequestBody body = RequestBody.create(JSON_TYPE, json);
+
+                Request request = new Request.Builder()
+                        .url("https://solarsunrise.ru/api/v1/login")
+                        .method("POST", body)
+                        .build();
+
+                Log.println(Log.DEBUG, "DEBUG", json);
+                Log.println(Log.DEBUG, "DEBUG", body.toString());
+                Log.println(Log.DEBUG, "DEBUG", request.toString());
+                Log.println(Log.DEBUG, "DEBUG", request.body().toString());
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                        System.out.println(response.body().string());
+                    }
+                });
             }
         });
     }
@@ -64,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
             return false;
         } else {
             textInputPassword.setError(null);
-            return  true;
+            return true;
         }
     }
 
